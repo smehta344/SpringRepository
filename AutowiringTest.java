740e0cfbed7cc40f15d69e259b7c5b7d3c686a6e
SANJEEV <sanjeev>
2019-04-07 23:10:14 +0530

    Test class modify

--- AutowiringTest.java
+++ AutowiringTest.java
@@ -7,10 +7,9 @@ import org.springframework.core.io.ClassPathResource;
 import com.aw.beans.Robot;
 
 public class AutowiringTest {
-
 	public static void main(String[] args) {
 		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("com/aw/common/application-context.xml"));
 		Robot robot = factory.getBean("robot", Robot.class);
-		System.out.println(robot);
+		System.out.println("Robot:" +robot);
 	}
 }
